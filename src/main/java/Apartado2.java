5cf3a1b05e18da26eb554adf8cf8b6c320d9e236
sergiioo101 <smartros@myuax.com>
2024-05-03 20:53:51 +0200

    2

---
+++ src/main/java/Apartado2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args) {
+        List<Double> arrayList = new ArrayList<>();
+        List<Double> linkedList = new LinkedList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            double randomNumber = random.nextDouble();
+            arrayList.add(randomNumber);
+            linkedList.add(randomNumber);
+        }
+
+        System.out.println("ArrayList:");
+        for (Double number : arrayList) {
+            System.out.println(number);
+        }
+
+        System.out.println("\nLinkedList:");
+        for (Double number : linkedList) {
+            System.out.println(number);
+        }
+    }
+}
